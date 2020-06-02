b307e76998c3d96c1ce73e5f06f5b097f31ad041
FIRST_NAME LAST_NAME <andre.oikawa@hotmail.com>
2020-06-02 16:56:29 -0400

    created all shapes

--- src/piece.js
+++ src/piece.js
@@ -34,8 +34,99 @@ class I extends Piece {
         else arr.push(this.cells[3]);
         return arr;
     }
-
-    
 }
 
+class J extends Piece {
+    constructor(type) {
+        super("j");
+        this.cells = [  [0,10,11,12],
+                        [1,11,21,2],
+                        [10,11,12,22],
+                        [20,21,11,1]
+                    ];
+    }
+
+    getBottomParts() {
+       // TODO
+    }
+}
+
+class L extends Piece {
+    constructor(type) {
+        super("l");
+        this.cells = [  [10,11,12,2],
+                        [1,11,21,22],
+                        [20,10,11,12],
+                        [0,1,11,21]
+                    ];
+    }
+
+    getBottomParts() {
+        // TODO
+    }
+}
+
+class O extends Piece {
+    constructor(type) {
+        super("o");
+        this.cells = [  [1,2,11,12],
+                        [1,2,11,12],
+                        [1,2,11,12],
+                        [1,2,11,12]
+                    ];
+    }
+
+    getBottomParts() {
+        // TODO
+    }
+}
+
+class S extends Piece {
+    constructor(type) {
+        super("s");
+        this.cells = [  [10,11,1,2],
+                        [1,11,12,22],
+                        [20,21,11,12],
+                        [0,10,11,21]
+                    ];
+    }
+
+    getBottomParts() {
+        // TODO
+    }
+}
+class T extends Piece {
+    constructor(type) {
+        super("t");
+        this.cells = [  [1,10,11,12],
+                        [1,11,21,12],
+                        [10,11,21,12],
+                        [10,1,11,21]
+                    ];
+    }
+
+    getBottomParts() {
+        // TODO
+    }
+}
+class Z extends Piece {
+    constructor(type) {
+        super("z");
+        this.cells = [  [0,1,11,12],
+                        [21,11,12,2],
+                        [10,11,21,22],
+                        [20,10,11,1]
+                    ];
+    }
+
+    getBottomParts() {
+        // TODO
+    }
+}
 exports.I = I
+exports.J = J
+exports.L = L
+exports.O = O
+exports.S = S
+exports.T = T
+exports.Z = Z
